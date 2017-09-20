d0d45a282288683c6d6707049c6042b2da75b105
hcl14 <hcl14@i.ua>
2017-09-20 21:30:37 +0300

    used mfcc+dtw example, little bit better, but noise is not discriminable

---
+++ mfcc_compare.py
@@ -0,0 +1,135 @@
+#https://github.com/d4r3topk/comparing-audio-files-python/blob/master/mfcc.py
+
+#http://www.speech.zone/exercises/dtw-in-python/
+
+#https://github.com/pierre-rouanet/dtw
+
+#https://github.com/pierre-rouanet/dtw/blob/master/examples/MFCC%20%2B%20DTW.ipynb
+
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+#from dtw import dtw
+from numpy.linalg import norm
+import scipy
+
+#************************************************************************
+#https://github.com/pierre-rouanet/dtw/blob/master/dtw.py
+from numpy import array, zeros, argmin, inf, equal, ndim
+from scipy.spatial.distance import cdist
+
+def dtw(x, y, dist):
+    """
+    Computes Dynamic Time Warping (DTW) of two sequences.
+    :param array x: N1*M array
+    :param array y: N2*M array
+    :param func dist: distance used as cost measure
+    Returns the minimum distance, the cost matrix, the accumulated cost matrix, and the wrap path.
+    """
+    assert len(x)
+    assert len(y)
+    r, c = len(x), len(y)
+    D0 = zeros((r + 1, c + 1))
+    D0[0, 1:] = inf
+    D0[1:, 0] = inf
+    D1 = D0[1:, 1:] # view
+    for i in range(r):
+        for j in range(c):
+            D1[i, j] = dist(x[i], y[j])
+    C = D1.copy()
+    for i in range(r):
+        for j in range(c):
+            D1[i, j] += min(D0[i, j], D0[i, j+1], D0[i+1, j])
+    if len(x)==1:
+        path = zeros(len(y)), range(len(y))
+    elif len(y) == 1:
+        path = range(len(x)), zeros(len(x))
+    else:
+        path = _traceback(D0)
+    return D1[-1, -1] / sum(D1.shape), C, D1, path
+
+def fastdtw(x, y, dist):
+    """
+    Computes Dynamic Time Warping (DTW) of two sequences in a faster way.
+    Instead of iterating through each element and calculating each distance,
+    this uses the cdist function from scipy (https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.distance.cdist.html)
+    :param array x: N1*M array
+    :param array y: N2*M array
+    :param string or func dist: distance parameter for cdist. When string is given, cdist uses optimized functions for the distance metrics.
+    If a string is passed, the distance function can be 'braycurtis', 'canberra', 'chebyshev', 'cityblock', 'correlation', 'cosine', 'dice', 'euclidean', 'hamming', 'jaccard', 'kulsinski', 'mahalanobis', 'matching', 'minkowski', 'rogerstanimoto', 'russellrao', 'seuclidean', 'sokalmichener', 'sokalsneath', 'sqeuclidean', 'wminkowski', 'yule'.
+    Returns the minimum distance, the cost matrix, the accumulated cost matrix, and the wrap path.
+    """
+    assert len(x)
+    assert len(y)
+    if ndim(x)==1:
+        x = x.reshape(-1,1)
+    if ndim(y)==1:
+        y = y.reshape(-1,1)
+    r, c = len(x), len(y)
+    D0 = zeros((r + 1, c + 1))
+    D0[0, 1:] = inf
+    D0[1:, 0] = inf
+    D1 = D0[1:, 1:]
+    D0[1:,1:] = cdist(x,y,dist)
+    C = D1.copy()
+    for i in range(r):
+        for j in range(c):
+            D1[i, j] += min(D0[i, j], D0[i, j+1], D0[i+1, j])
+    if len(x)==1:
+        path = zeros(len(y)), range(len(y))
+    elif len(y) == 1:
+        path = range(len(x)), zeros(len(x))
+    else:
+        path = _traceback(D0)
+    return D1[-1, -1] / sum(D1.shape), C, D1, path
+
+def _traceback(D):
+    i, j = array(D.shape) - 2
+    p, q = [i], [j]
+    while ((i > 0) or (j > 0)):
+        tb = argmin((D[i, j], D[i, j+1], D[i+1, j]))
+        if (tb == 0):
+            i -= 1
+            j -= 1
+        elif (tb == 1):
+            i -= 1
+        else: # (tb == 2):
+            j -= 1
+        p.insert(0, i)
+        q.insert(0, j)
+    return array(p), array(q)
+
+#************************************************************************
+
+
+
+
+
+def compare_librosa_wav_files(wav1,wav2,usenorm="l2"):
+
+    #Loading audio files
+    y1, sr1 = wav1[0],wav1[1] #librosa.load('out/1-6988-7076.wav') 
+    y2, sr2 = wav2[0],wav2[1] #librosa.load('out/2-14068-14168.wav') 
+
+    #Showing multiple plots using subplot
+    #plt.subplot(1, 2, 1) 
+    mfcc1 = librosa.feature.mfcc(y1,sr1)   #Computing MFCC values
+    #librosa.display.specshow(mfcc1)
+
+    #plt.subplot(1, 2, 2)
+    mfcc2 = librosa.feature.mfcc(y2, sr2)
+    #librosa.display.specshow(mfcc2)
+
+    if usenorm=="l2":
+        d = lambda x, y: norm(x - y, ord=1)
+    if usenorm=="cos":
+        d = lambda x, y: scipy.spatial.distance.cosine(x,y)
+        
+    dist, cost, acc, path = fastdtw(mfcc1.T, mfcc2.T, dist=d)
+    #print("The normalized distance between the two : ",dist)   # 0 for similar audios 
+
+    #plt.imshow(cost.T, origin='lower', cmap=plt.get_cmap('gray'), interpolation='nearest')
+    #plt.plot(path[0], path[1], 'w')   #creating plot for DTW
+
+    #plt.show() #To display the plots graphically 
+    return dist
---
+++ mfcc_features2.py
@@ -0,0 +1,78 @@
+#https://stackoverflow.com/questions/25988749/mfcc-feature-descriptors-for-audio-classification-using-librosa
+
+#https://dsp.stackexchange.com/questions/27417/comparing-mfcc-features-what-do-they-represent
+
+# parameters description:
+# https://github.com/cournape/talkbox/blob/master/scikits/talkbox/features/mfcc.py
+
+# another mfcc impleme3ntation:
+# https://github.com/jameslyons/python_speech_features
+
+import os
+import sys
+
+path = 'out/'
+
+wavfiles = []
+# read file list
+for file in os.listdir(path):
+    current = os.path.join(path, file)
+    if os.path.isfile(current):
+        if current.split(".")[-1]=="wav":
+            wavfiles.append(current)
+
+
+import numpy as np
+#import scipy.io.wavfile
+#from scikits.talkbox.features import mfcc
+
+from mfcc_compare import *
+
+
+# process files
+
+mfcc_descriptors = []
+
+for wavfile in sorted(wavfiles):
+
+    print(wavfile)
+    
+    
+    wav = [0,0]
+    wav[0],wav[1] = librosa.load(wavfile)
+    
+    
+    mfcc_descriptors.append((wavfile,wav))
+    
+    
+#print(mfcc_descriptors)
+
+
+# apply similarity measure
+
+# we need to do k-means clustering there which is available in scipy
+
+# but at the moment lets just compute matrix for L2 and cosine
+
+number_of_records = len(mfcc_descriptors)
+
+
+print
+print
+
+
+L2 = np.zeros((number_of_records,number_of_records))
+
+for i in range(0,number_of_records):
+    for j in range(0,number_of_records):
+        L2[i,j] = compare_librosa_wav_files(mfcc_descriptors[i][1], mfcc_descriptors[j][1],"l2")
+        
+L2 = np.vstack((range(1,14),L2))
+
+print np.transpose(np.array(range(1,14)))
+L2 = np.append(np.array(range(0,14)).reshape(14,1),L2,axis=1)
+        
+print("Normalized L2 distances matrix:\n")
+np.set_printoptions(precision=2,linewidth=200)
+print(L2)
+
